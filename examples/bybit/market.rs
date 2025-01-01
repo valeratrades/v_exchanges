001196a35b99e96d8f688b2283de66d1f707e694
valeratrades <v79166789533@gmail.com>
2025-01-01 19:59:47 +0000

    wip

--- examples/bybit/market.rs
+++ examples/bybit/market.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 use v_exchanges::{bybit::Client, core::Exchange};
-use v_exchanges_adapters::binance::{BinanceHttpUrl, BinanceOption};
+use v_exchanges_adapters::bybit::{BybitHttpAuth, BybitHttpUrl, BybitOption};
 
 #[tokio::main]
 async fn main() {
@@ -9,6 +9,20 @@ async fn main() {
 	v_utils::utils::init_subscriber(v_utils::utils::LogDestination::xdg("v_exchanges"));
 
 	let mut client = Client::new();
+
+	let ticker: serde_json::Value = client
+		.get("/v5/market/tickers", &[("category", "spot"), ("symbol", "BTCUSDT")], [BybitOption::Default])
+		.await
+		.expect("failed to get ticker");
+	println!("Ticker:\n{ticker}");
+
+	if let (Ok(key), Ok(secret)) = (env::var("BYBIT_TIGER_READ_KEY"), env::var("BYBIT_TIGER_READ_SECRET")) {
+		client.update_default_option(BybitOption::Key(key));
+		client.update_default_option(BybitOption::Secret(secret));
+		private(&client).await;
+	} else {
+		eprintln!("BYBIT_TIGER_READ_KEY or BYBIT_TIGER_READ_SECRET is missing, skipping private API methods.");
+	}
 
 	//client.update_default_option(BinanceOption::HttpUrl(BinanceHttpUrl::FuturesUsdM));
 	//let klines = client.futures_klines(("BTC", "USDT").into(), "1m".into(), 2, None, None).await.unwrap();
@@ -23,5 +37,12 @@ async fn main() {
 	//} else {
 	//	eprintln!("BINANCE_TIGER_READ_KEY or BINANCE_TIGER_READ_SECRET is missing, skipping private API methods.");
 	//}
-	todo!();
+}
+
+async fn private(client: &Client) {
+	let balance: serde_json::Value = client
+		.get("/v5/account/wallet-balance", &[("accountType", "UNIFIED")], [BybitOption::HttpAuth(BybitHttpAuth::V3AndAbove)])
+		.await
+		.expect("failed to get balance");
+	println!("Balance:\n{balance}");
 }
--- v_exchanges/src/bybit/account.rs
+++ v_exchanges/src/bybit/account.rs
@@ -3,5 +3,6 @@ use color_eyre::eyre::Result;
 use crate::core::AssetBalance;
 
 pub async fn balances(client: &v_exchanges_adapters::Client) -> Result<Vec<AssetBalance>> {
+	println!("bybit::account::balances");
 	todo!();
 }
