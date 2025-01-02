099d935b6e817c53e5a3cfb1df8ea383b3fda020
valeratrades <v79166789533@gmail.com>
2025-01-02 09:00:43 +0000

    fix: _

--- examples/binance/market.rs
+++ examples/binance/market.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 use v_exchanges::{binance::Client, core::Exchange};
-use v_exchanges_adapters::binance::{BinanceHttpUrl, BinanceOption};
+use v_exchanges_adapters::binance::{self, BinanceHttpUrl, BinanceOption};
 
 #[tokio::main]
 async fn main() {
@@ -11,14 +11,19 @@ async fn main() {
 	let mut client = Client::new();
 
 	client.update_default_option(BinanceOption::HttpUrl(BinanceHttpUrl::FuturesUsdM));
-	let klines = client.futures_klines(("BTC", "USDT").into(), "1m".into(), 2, None, None).await.unwrap();
-	let price = client.futures_price(("BTC", "USDT").into()).await.unwrap();
+
+	//before implementing the trait for bybit too, was able to just do eg: `let klines = client.futures_klines(("BTC", "USDT").into(), "1m".into(), 2, None, None).await.unwrap();`
+
+	let klines = <Client as Exchange<binance::BinanceOptions>>::futures_klines(&client, ("BTC", "USDT").into(), "1m".into(), 2, None, None)
+		.await
+		.unwrap();
+	let price = <Client as Exchange<binance::BinanceOptions>>::futures_price(&client, ("BTC", "USDT").into()).await.unwrap();
 	dbg!(&klines, price);
 
 	if let (Ok(key), Ok(secret)) = (env::var("BINANCE_TIGER_READ_KEY"), env::var("BINANCE_TIGER_READ_SECRET")) {
 		client.update_default_option(BinanceOption::Key(key));
 		client.update_default_option(BinanceOption::Secret(secret));
-		let balance = client.futures_asset_balance("USDT".into()).await.unwrap();
+		let balance = <Client as Exchange<binance::BinanceOptions>>::futures_asset_balance(&client, "USDT".into()).await.unwrap();
 		dbg!(&balance);
 	} else {
 		eprintln!("BINANCE_TIGER_READ_KEY or BINANCE_TIGER_READ_SECRET is missing, skipping private API methods.");
--- v_exchanges/src/bybit/account.rs
+++ v_exchanges/src/bybit/account.rs
@@ -1,8 +1,47 @@
+use std::str::FromStr;
+
 use color_eyre::eyre::Result;
+use serde::{Deserialize, Serialize};
 
 use crate::core::AssetBalance;
 
 pub async fn balances(client: &v_exchanges_adapters::Client) -> Result<Vec<AssetBalance>> {
 	println!("bybit::account::balances");
 	todo!();
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum AccountType {
+	Spot,
+	Contract,
+	Unified,
+	Funding,
+	Option,
+}
+impl std::fmt::Display for AccountType {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		let s = serde_plain::to_string(self).map_err(|_| std::fmt::Error)?;
+		write!(f, "{s}")
+	}
+}
+impl FromStr for AccountType {
+	type Err = ();
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		serde_plain::from_str(s).map_err(|_| ())
+	}
+}
+
+mod tests {
+	use insta;
+
+	use super::*;
+
+	#[test]
+	fn test_account_type_serde() {
+		insta::assert_debug_snapshot!(format!("{}", AccountType::Unified), @r#""UNIFIED""#);
+		let s = "UNIFIED";
+		let _: AccountType = s.parse().unwrap();
+	}
 }
