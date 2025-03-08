cb17b8f528286ff784c62c5a71b10a73a4a69b81
valeratrades <valeratrades@gmail.com>
2025-03-08 18:40:36 +0000

    _

--- flake.nix
+++ flake.nix
@@ -54,27 +54,27 @@
           shellHook =
             pre-commit-check.shellHook +
             ''
-              mkdir -p ./.github/workflows
-              rm -f ./.github/workflows/errors.yml; cp ${workflowContents.errors} ./.github/workflows/errors.yml
-              rm -f ./.github/workflows/warnings.yml; cp ${workflowContents.warnings} ./.github/workflows/warnings.yml
+                            mkdir -p ./.github/workflows
+                            rm -f ./.github/workflows/errors.yml; cp ${workflowContents.errors} ./.github/workflows/errors.yml
+                            rm -f ./.github/workflows/warnings.yml; cp ${workflowContents.warnings} ./.github/workflows/warnings.yml
 
-              cp -f ${v-utils.files.licenses.blue_oak} ./LICENSE
+                            cp -f ${v-utils.files.licenses.blue_oak} ./LICENSE
 
-              cargo -Zscript -q ${v-utils.hooks.appendCustom} ./.git/hooks/pre-commit
-              cp -f ${(v-utils.hooks.treefmt) {inherit pkgs;}} ./.treefmt.toml
-              cp -f ${(v-utils.hooks.preCommit) { inherit pkgs pname; }} ./.git/hooks/custom.sh
+                            cargo -Zscript -q ${v-utils.hooks.appendCustom} ./.git/hooks/pre-commit
+                            cp -f ${(v-utils.hooks.treefmt) {inherit pkgs;}} ./.treefmt.toml
+                            cp -f ${(v-utils.hooks.preCommit) { inherit pkgs pname; }} ./.git/hooks/custom.sh
 
-              mkdir -p ./.cargo
-              cp -f ${(v-utils.files.rust.config {inherit pkgs;})} ./.cargo/config.toml #dbg
-              cp -f ${(v-utils.files.rust.toolchain {inherit pkgs;})} ./.cargo/rust-toolchain.toml
-              cp -f ${(v-utils.files.rust.rustfmt {inherit pkgs;})} ./rustfmt.toml
-              cp -f ${(v-utils.files.rust.deny {inherit pkgs;})} ./deny.toml
-              cp -f ${(v-utils.files.gitignore { inherit pkgs; langs = ["rs"];})} ./.gitignore
+                            mkdir -p ./.cargo
+              							#cp -f ${(v-utils.files.rust.config {inherit pkgs;})} ./.cargo/config.toml #dbg
+                            cp -f ${(v-utils.files.rust.toolchain {inherit pkgs;})} ./.cargo/rust-toolchain.toml
+                            cp -f ${(v-utils.files.rust.rustfmt {inherit pkgs;})} ./rustfmt.toml
+                            cp -f ${(v-utils.files.rust.deny {inherit pkgs;})} ./deny.toml
+                            cp -f ${(v-utils.files.gitignore { inherit pkgs; langs = ["rs"];})} ./.gitignore
 
-              cp -f ${readme} ./README.md
+                            cp -f ${readme} ./README.md
             '';
 
-          packages = [
+          buildInputs = [
             mold-wrapped
             openssl
             pkg-config
